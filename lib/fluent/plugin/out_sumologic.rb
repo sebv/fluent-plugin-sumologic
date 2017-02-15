db01170885bd03984bc4660ae8558eb9d496bcbf
sebv <seb.vincent@gmail.com>
2017-02-16 00:05:45 +0800

    fixes encoding on the fly

--- lib/fluent/plugin/out_sumologic.rb
+++ lib/fluent/plugin/out_sumologic.rb
@@ -38,6 +38,32 @@ class Fluent::SumologicOutput< Fluent::BufferedOutput
     super
   end
 
+  def fix_encoding(crap_str)
+    # Tries to figure out encoding so that conversion
+    # to UTF-8 does not crash. If all fails removes
+    # the bad characters.
+    if crap_str.nil?
+      return crap_str
+    end
+
+    begin
+      return crap_str.encode(Encoding.find('UTF-8'))
+    rescue
+    end
+    encodings = Array["ASCII-8BIT", "UTF-8", "ISO-8859-1", "ISO-8859-15"]
+    orig_enc = crap_str.encoding
+    encodings.each do |enc|
+      begin
+        res = crap_str.encode(Encoding.find('UTF-8'), Encoding.find(enc))
+        crap_str.force_encoding(enc)
+        log.debug("Fixed encoding from #{orig_enc} to #{enc} " +
+                 "for #{crap_str}")
+        return res
+      rescue
+      end
+    end
+  end
+
   def write(chunk)
     messages_list = {}
 
@@ -52,8 +78,22 @@ class Fluent::SumologicOutput< Fluent::BufferedOutput
           end
           source_name = record[@source_name_key] || ''
           record.delete(@source_name_key)
+
+          json_record = nil
+          begin
+            json_record = record.to_json
+          rescue
+            # When a record is badly encoded, we try
+            # to fix the mess.
+            clean_record = Hash.new
+            record.each do |k, v|
+              clean_record[k] = self.fix_encoding(v)
+            end
+            json_record = clean_record.to_json
+          end
+
           messages_list[source_name] = [] unless messages_list[source_name]
-          messages_list[source_name] << record.to_json
+          messages_list[source_name] << json_record
         end
       when 'text'
         chunk.msgpack_each do |tag, time, record|
